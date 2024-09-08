f23631481a82eb7333b72b1c85e184b7207b13ec
DewaNandaTM <162023845+DewaNandaTM@users.noreply.github.com>
2024-09-08 20:47:16 +0700

    Create class.java

---
+++ Tugas 2/class.java
@@ -0,0 +1,11 @@
+public class car {
+  private string brand;
+  private string model;
+  private string color;
+
+  public void viewCarInfo {
+    system.out.println(brand);
+    system.out.println(model);
+    system.out.println(color);
+  }
+}
