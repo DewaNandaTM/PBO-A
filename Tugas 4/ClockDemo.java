5756dd07178b6f4b84236f2d884e94f538ace8f6
DewaNandaTM <162023845+DewaNandaTM@users.noreply.github.com>
2024-09-23 20:24:54 +0700

    Create ClockDemo.java

---
+++ Tugas 4/ClockDemo.java
@@ -0,0 +1,15 @@
+public class ClockDemo
+{
+    public static void main (String[] args) throws InterruptedException
+    {
+        ClockDisplay clock = new ClockDisplay();
+        clock.setTime(20,30,0);
+        
+        while(true){
+            clock.minIncrement();
+            System.out.println(" tick...");
+            System.out.println("Current time: " + clock.getTime());
+            Thread.sleep(999);
+        }
+    }
+}
