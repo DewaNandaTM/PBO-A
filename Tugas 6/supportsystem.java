67190aa09a769ba3e95cad1a109819724e52a10b
DewaNandaTM <162023845+DewaNandaTM@users.noreply.github.com>
2024-10-14 22:47:56 +0700

    Create supportsystem.java

---
+++ Tugas 6/supportsystem.java
@@ -0,0 +1,27 @@
+public class SupportSystem {
+  private InputReader reader;
+  private Responder responder;
+
+  public SupportSystem() {
+    reader = new InputReader();
+    responder = new Responder();
+  }
+
+  public void start() {
+    System.out.println("Welcome to Riot Technical Support System.\n");
+    System.out.println("Please tell us about your problem.");
+    System.out.println("We will assist you with any problem you might have.");
+    System.out.println("Please type 'bye' to exit our system.");
+
+    while (true) {
+      String input = reader.getInput();
+      if (input.equalsIgnoreCase("bye")) {
+        System.out.println("Thank you for using our technical support system, Goodbye!");
+        break;
+      } 
+      
+      String response = responder.generateResponse(input);
+      System.out.println(response);
+    }
+  }
+}
