ab9e1a6a9ae33f4d9d74631cebac44bb00f15878
DewaNandaTM <162023845+DewaNandaTM@users.noreply.github.com>
2024-11-06 19:56:26 +0700

    Update supportsystem.java

--- Tugas 6/supportsystem.java
+++ Tugas 6/supportsystem.java
@@ -1,27 +1,26 @@
 public class SupportSystem {
-  private InputReader reader;
-  private Responder responder;
-
-  public SupportSystem() {
-    reader = new InputReader();
-    responder = new Responder();
-  }
-
-  public void start() {
-    System.out.println("Welcome to Riot Technical Support System.\n");
-    System.out.println("Please tell us about your problem.");
-    System.out.println("We will assist you with any problem you might have.");
-    System.out.println("Please type 'bye' to exit our system.");
-
-    while (true) {
-      String input = reader.getInput();
-      
-      String response = responder.generateResponse(input);
-      System.out.println(response);
-
-      if (input.equalsIgnoreCase("bye")) {
-        break;
-      } 
+    private InputReader reader;
+    private Responder responder;
+  
+    public SupportSystem() {
+      reader = new InputReader();
+      responder = new Responder();
+    }
+  
+    public void start() {
+      System.out.println("Welcome to Riot Technical Support System.\n");
+      System.out.println("Please tell us about your problem.");
+      System.out.println("We will assist you with any problem you might have.");
+      System.out.println("Please type 'bye' to exit our system.");
+  
+      while (true) {
+        String input = reader.getInput();
+        
+        responder.generateResponse(input);
+  
+        if (input.equalsIgnoreCase("bye")) {
+          break;
+        } 
+      }
     }
   }
-}
