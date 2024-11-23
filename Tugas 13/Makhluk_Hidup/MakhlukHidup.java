a0b93523a7727d88f1a0b7a442bff1515691fbc6
DewaNandaTM <162023845+DewaNandaTM@users.noreply.github.com>
2024-11-23 20:47:00 +0700

    Create MakhlukHidup.java

---
+++ Tugas 13/Makhluk_Hidup/MakhlukHidup.java
@@ -0,0 +1,10 @@
+public abstract class MakhlukHidup {
+    protected String name;
+
+    public MakhlukHidup(String name) {
+        this.name = name;
+    }
+
+    public abstract void breathe();
+    public abstract void grow();
+}
